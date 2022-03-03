98d7b03f515e4c5a0ee8f5fe828efbfad134b3cd
renardelyon <renard.elyon.r@gmail.com>
2022-03-03 14:58:18 +0700

    Adding webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: path.resolve(__dirname, 'dist')
+  },
+};
